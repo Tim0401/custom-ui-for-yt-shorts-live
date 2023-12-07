e60fe40cd1ec633e35e5409f8695c40f5c8fe5c3
Tsukasa Murata <36380433+Tim0401@users.noreply.github.com>
2023-12-08 00:28:50 +0900

    Update context.user.js

--- context.user.js
+++ context.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Custom UI for YouTube Shorts Live (Context Menu)
-// @version      0.1
+// @version      0.2
 // @namespace    https://gist.github.com/Tim0401
 // @description  Custon UI for watching YouTube Shorts style live stream
 // @author       Tim0401
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @require      https://raw.githubusercontent.com/Tim0401/custom-ui-for-yt-shorts-live/main/common.js
+// @match        https://www.youtube.com/*
 // @run-at       context-menu
+// @require      https://raw.githubusercontent.com/Tim0401/custom-ui-for-yt-shorts-live/main/common.js
 // @grant        none
 // ==/UserScript==
 
